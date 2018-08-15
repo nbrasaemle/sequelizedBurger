bd8e978fed450df0f3b504d69db21c2b3497fcce
nbrasaemle <nicole.brasaemle@gmail.com>
2018-08-15 16:43:32 -0500

    second initial commit

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,23 @@
-const orm = require("../config/orm.js");
+// const orm = require("../config/orm.js");
 
-var burger = {
-    all: function(cb){
-        orm.all("burgers", function (res) {
-            cb(res);
-        })
-    },
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers",  objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
-};
+// var burger = {
+//     all: function(cb){
+//         orm.all("burgers", function (res) {
+//             cb(res);
+//         })
+//     },
+//     create: function (cols, vals, cb) {
+//         orm.create("burgers", cols, vals, function (res) {
+//             cb(res);
+//         });
+//     },
+//     update: function (objColVals, condition, cb) {
+//         orm.update("burgers",  objColVals, condition, function (res) {
+//             cb(res);
+//         });
+//     }
+// };
 
-// Export the database functions for the controller (burgersController.js)
-module.exports = burger;
+// // Export the database functions for the controller (burgersController.js)
+// module.exports = burger;
 
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+
+var db = require("./models");
 
 app.use(express.static("public"));
 
@@ -18,7 +21,13 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+// app.listen(PORT, function () {
+//     // Log (server-side) when our server has started
+//     console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
